8347da456cdc3b757d12a71a7c8b05aaeee1f995
TimTmi <khoanart05@gmail.com>
2026-05-28 00:45:01 +0700

    refactor: extract stats from config

--- characters/character_config.gd
+++ characters/character_config.gd
@@ -2,12 +2,6 @@ class_name CharacterConfig extends Resource
 
 
 @export var scene: PackedScene
-
-@export var health: float = 100.0
-@export var attack_percent: float = 0.0
-@export var attack_flat: float = 0.0
-@export var defense_percent: float = 0.0
-@export var defense_flat: float = 0.0
-
+@export var stats: CharacterStats
 @export var slot_abilities: Dictionary[AbilitySystem.CommandSlot, Ability]
 @export var passive_abilities: Array[Ability]
---
+++ characters/stats_config.gd
@@ -0,0 +1,8 @@
+class_name CharacterStats extends Resource
+
+
+@export var health: float = 100.0
+@export var attack_percent: float = 0.0
+@export var attack_flat: float = 0.0
+@export var defense_percent: float = 0.0
+@export var defense_flat: float = 0.0
